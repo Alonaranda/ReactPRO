8e9db19a88ad0006dbce690a822b5c0ab1bf31db
Christian Alonso <christian.alonso.-nd@disney.com>
2022-02-01 19:06:29 -0600

    Refactor

--- react-app-rrd-v5/src/Component-Patterns/pages/ShoopingPage.tsx
+++ react-app-rrd-v5/src/Component-Patterns/pages/ShoopingPage.tsx
@@ -13,27 +13,17 @@ export const ShoopingPage = () => {
             <ProductCard
                 key={product.id}
                 product={product}
-                className="bg-dark text-white"
                 initialValues={{
                     count: 0,
-                    maxCount: 6,
+                    //maxCount: 6,
                 }}
             >
                 {
                     ({reset, count, increaseBy, isMaxCountReaced, maxCount}) => (
                         <>
-                            <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px 10px rgb(0, 0, 0, 0.2' }} />
-                            <ProductTitle className="text-bold" />
-                            <ProductButtons className="custom-buttons" />
-
-                            <button onClick={reset}>Reset</button>
-
-                            <button onClick={() => increaseBy(-2)} >-2</button>
-                            {
-                                (!isMaxCountReaced && <button onClick={() => increaseBy(2)}  >+2</button>)
-                            }
-                            
-                            <span>{count} - {maxCount}</span>
+                            <ProductImage />
+                            <ProductTitle />
+                            <ProductButtons />
                         </>
                     )
                 }

279aae34a57699903441dc46a3bd772ecf507bc6
Christian Alonso <christian.alonso@wizeline.com>
2022-03-03 11:12:46 -0600

    Workbox terminado

---
+++ react-calendar-frontend/workbox-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	globDirectory: 'build/',
+	globPatterns: [
+		'**/*.{json,ico,html,png,txt,css,js}'
+	],
+	swDest: 'build/sw.js',
+	swSrc: 'src/sw-template.js', // generateSW no funciona con esta propiedad: 
+	injectionPoint: '__WB_MANIFEST'
+	// ignoreURLParametersMatching: [
+	// 	/^utm_/,
+	// 	/^fbclid$/
+	// ]
+};
